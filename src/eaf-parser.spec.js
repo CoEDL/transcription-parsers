af5449e44ccd29789e1739b43c02a79970bd2eec
Marco La Rosa <m@lr.id.au>
2020-02-18 14:39:13 +1100

    can't test eafparser directly given change to have json passed in

--- src/eaf-parser.spec.js
+++ src/eaf-parser.spec.js
@@ -1,4 +1,3 @@
-const { EAFParser } = require("./eaf-parser");
 const { Parser } = require("./index");
 const fs = require("fs");
 const path = require("path");
@@ -9,24 +8,13 @@ it("should be able to parse eaf test file 1 - single tier", async () => {
         "utf-8"
     );
 
-    // test parsing with the EAF parser directly
-    let parser = new EAFParser();
-    let result = await parser.parse({ data });
-    expect(result.tiers.name).toBe("tiers");
-    expect(result.tiers.children.length).toBe(1);
-    expect(result.tiers.children[0].name).toBe("id@unknown");
-    expect(result.timeslots.name).toBe("timeslots");
-    expect(result.timeslots.children.length).toBe(1);
-    expect(result.timeslots.children[0].name).toBe("ts1");
-
-    // test parsing with the main parser
     parser = new Parser({ name: "single-tier.eaf", data });
     result = await parser.parse();
     expect(result.tiers.name).toBe("tiers");
     expect(result.tiers.children.length).toBe(1);
     expect(result.tiers.children[0].name).toBe("id@unknown");
     expect(result.timeslots.name).toBe("timeslots");
-    expect(result.timeslots.children.length).toBe(1);
+    expect(result.timeslots.children.length).toBe(2);
     expect(result.timeslots.children[0].name).toBe("ts1");
 });
 
@@ -36,15 +24,8 @@ it("should be able to parse eaf test file 2 - multiple tiers", async () => {
         "utf-8"
     );
 
-    // test parsing with the EAF parser directly
-    let parser = new EAFParser();
-    let result = await parser.parse({ data });
-    expect(result.tiers.children.length).toBe(6);
-    expect(result.timeslots.children.length).toBe(1);
-
-    // test parsing with the main parser
     parser = new Parser({ name: "single-tier.eaf", data });
     result = await parser.parse();
     expect(result.tiers.children.length).toBe(6);
-    expect(result.timeslots.children.length).toBe(1);
+    expect(result.timeslots.children.length).toBe(2);
 });
