d7f59533d2c18703c2ba32e5c0f444db4a039281
Marco La Rosa <m@lr.id.au>
2020-10-07 17:30:05 +1100

    formatting; bugfix

--- src/flextext-parser.js
+++ src/flextext-parser.js
@@ -7,36 +7,38 @@ class FlextextParser {
 
     parse({ data }) {
         let paragraphs = data.elements[0].elements[0].elements[0].elements;
-        paragraphs = paragraphs.map(paragraph => {
-            let phrases = paragraph.elements[0].elements;
-            phrases = phrases.map(phrase => {
-                let items = phrase.elements.filter(e => e.name === "item");
-                let words = phrase.elements.filter(e => e.name === "words")[0]
+        paragraphs = paragraphs.map((paragraph) => {
+            let phrases = paragraph.elements
+                ? paragraph.elements[0].elements
+                : [];
+            phrases = phrases.map((phrase) => {
+                let items = phrase.elements.filter((e) => e.name === "item");
+                let words = phrase.elements.filter((e) => e.name === "words")[0]
                     .elements;
-                items = items.map(item => {
+                items = items.map((item) => {
                     return {
                         lang: item.attributes.lang,
                         type: item.attributes.type,
-                        text: item.elements[0].text
+                        text: item.elements ? item.elements[0].text : "",
                     };
                 });
-                let transcription = items.filter(i => i.type !== "text")[0];
-                let translation = items.filter(i => i.type === "gls")[0];
+                let transcription = items.filter((i) => i.type !== "text")[0];
+                let translation = items.filter((i) => i.type === "gls")[0];
 
-                words = words.map(word => {
+                words = words.map((word) => {
                     let morphemes = word.elements.filter(
-                        e => e.name === "morphemes"
+                        (e) => e.name === "morphemes"
                     );
                     if (!morphemes.length) return { morphemes: [] };
                     if (!morphemes[0].elements) return { morphemes: [] };
 
-                    morphemes = morphemes.map(morph => {
-                        return morph.elements.map(e => {
-                            return e.elements.map(f => {
+                    morphemes = morphemes.map((morph) => {
+                        return morph.elements.map((e) => {
+                            return e.elements.map((f) => {
                                 return {
                                     lang: getAttribute(f, "lang"),
                                     type: getAttribute(f, "type"),
-                                    text: f.elements[0].text
+                                    text: f.elements[0].text,
                                 };
                             });
                         });
@@ -53,11 +55,11 @@ class FlextextParser {
                             1000,
                         end:
                             parseInt(phrase.attributes["end-time-offset"]) /
-                            1000
+                            1000,
                     },
                     transcription,
                     translation,
-                    words
+                    words,
                 };
                 return phrase;
             });
@@ -76,5 +78,5 @@ function getAttribute(e, attr) {
 }
 
 module.exports = {
-    FlextextParser
+    FlextextParser,
 };
